de0001e3dee970332f904e8bcf2766d341c8bf6b
Ana Chicaiza <anachicaiza@icloud.com>
2024-09-18 14:22:46 +0100

    added blink function

--- src/index.js
+++ src/index.js
@@ -10,9 +10,12 @@ function generatePoem(event) {
   let topicElement = document.querySelector("#topic");
   let apiKey = "b739b64actfb7710ab2aa8f6044o4c38";
   let context =
-    "You are an expert writer and love to write poems. Your goal is to write a 4 line poem and display it in basic HTML structure, but without the html and body tags and separate each line with a </br>. Be sure of generate a different poem every time. Do not add a title";
+    "You are an expert writer and love to write poems. Your goal is to write a 4 line poem and display it in basic HTML structure, but without the html and body tags and separate each line. Be sure of generate a different poem every time. Do not add a title";
   let prompt = `Generate a poem about ${topicElement.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let poemContainer = document.querySelector("#poem");
+  poemContainer.classList.remove("hidden");
+  poemContainer.innerHTML = `<div class= "generating">⏳Generating poem about ${topicElement.value}</div>`;
   axios.get(apiURL).then(displayPoem);
 }
 let poemElement = document.querySelector("#poem-generator");
